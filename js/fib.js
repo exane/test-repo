c81920d859ac0d0122edfb75bf14dfe1ee92b3e1
Tim Meier <raco0n@gmx.de>
2016-02-05 10:49:53 +0100

    add test cfg

--- js/fib.js
+++ js/fib.js
@@ -1,4 +1,9 @@
+var importantConfigFile = require("./config/cfg.js");
+
+
 var fib = function(n) {
+	if(importantConfigFile.key != "abc123") 
+		throw Error();
 	if(n <= 1) 
 		return 1;
 	return fib(n-1) + fib(n-2);
