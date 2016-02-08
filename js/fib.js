302a895c47987c2b8f677acee6ee230219ce9203
Tim Meier <raco0n@gmx.de>
2016-02-08 09:35:40 +0100

    Update fib.js

--- js/fib.js
+++ js/fib.js
@@ -5,8 +5,10 @@ var importantConfigFile = JSON.parse(fs.readFileSync("./config/cfg.js", 'utf-8')
 
 
 var fib = function(n) {
-	if(importantConfigFile.key != "abc123") 
-		throw Error("WRONG KEY FROM CONFIG FILE");
+	if(importantConfigFile.key != "abc123") {
+		console.error("WRONG KEY FROM CONFIG FILE");
+		return;
+	}
 	if(n <= 1) 
 		return 1;
 	return fib(n-1) + fib(n-2);
