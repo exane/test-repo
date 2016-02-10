108981385b751cb096ce774488c2dc01665ad285
Tim Meier <raco0n@gmx.de>
2016-02-10 20:02:30 +0100

    Update fib.js

--- js/fib.js
+++ js/fib.js
@@ -3,10 +3,16 @@ var fs = require("fs");
 
 var importantConfigFile = JSON.parse(fs.readFileSync("./config/cfg.js", 'utf-8'));
 
+var secretTextFile = fs.readFileSync("./SECRET_TEXT.txt", 'utf-8');
+
 
 var fib = function(n) {
 	if(importantConfigFile.key != "abc123") {
 		console.error("WRONG KEY FROM CONFIG FILE");
+		return;
+	}
+	if(!secretTextFile) {
+		console.error("NO SECRET TEXT FILE");
 		return;
 	}
 	if(n <= 1) 
